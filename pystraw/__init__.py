990d80f775d23a99dc40ae69e96d7d21c0e1b348
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-08-30 21:49:55 +0300

    Watchdog thread pattern found

--- pystraw/__init__.py
+++ pystraw/__init__.py
@@ -1,32 +1,53 @@
 
 from pyxs import Client
-from threading import Thread
 
-def initialise():
-  global _xs
-  _xs = Client()
+import atexit
+import threading
+import time
 
-  global my_domid
-  my_domid = int(_xs.read("domid"))
-
-  straw_top = "data/straw"
-  _xs.mkdir(straw_top)
-  _xs.set_permissions(straw_top, [u'b' + str(my_domid)])
-
-  warts_top = "data/warts"
-  _xs.mkdir(warts_top)
-  _xs.set_permissions(warts_top, [u'r' + str(my_domid)])
-
-  monitor = _xs.monitor()
-  monitor.watch(straw_top, "token1")
- 
-  _watchdog = Thread(target=watchdog, args=[monitor], group=None)
+def setup():
+  print "register cleanup"
+  atexit.register(cleanup)
+  print "setup XS directories"
+  print "cleanup shared directories"
+  print "start watchdog thread"
+  _watchdog = threading.Thread(target=doggie)
+  _watchdog.setDaemon(True)
   _watchdog.start()
 
-def watchdog(monitor):
-  while True:
-    event = monitor.wait()
-    print event
+def cleanup():
+  print "cleanup shared directories"
 
-initialise()
+def doggie():
+  while True:
+    print "woof"
+    time.sleep(1)
+
+setup()
+
+#def initialise():
+#  global _xs
+#  _xs = Client()
+#
+#  global my_domid
+#  my_domid = int(_xs.read("domid"))
+#
+#  straw_top = "data/straw"
+#  _xs.mkdir(straw_top)
+#  _xs.set_permissions(straw_top, [u'b' + str(my_domid)])
+#
+#  warts_top = "data/warts"
+#  _xs.mkdir(warts_top)
+#  _xs.set_permissions(warts_top, [u'r' + str(my_domid)])
+#
+#  monitor = _xs.monitor()
+#  monitor.watch(straw_top, "token1")
+# 
+#  _watchdog = Thread(target=watchdog, args=[monitor], group=None)
+#  _watchdog.start()
+#
+#def watchdog(monitor):
+#  while True:
+#    event = monitor.wait()
+#    print event
 
