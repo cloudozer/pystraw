35b7128e5f5a526f70b1a3bd93e97f8a8a0e3ef8
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-09-03 15:34:55 +0300

    wip

--- pystraw/_pystraw/pystraw.c
+++ pystraw/_pystraw/pystraw.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <xen/grant_table.h>
 #include <xen/gntalloc.h>
+#include <xen/sys/gntalloc.h>
+#include <xen/sys/evtchn.h>
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <alloca.h>
 #include <assert.h>
 
 #define NUM_STRAW_REFS    8
@@ -26,33 +29,111 @@ struct straw_ring_t {
 	uint8_t output[STRAW_RING_SIZE];
 };
 
-void *pore_straw_open(domid_t domid, grant_ref_t refs[NUM_STRAW_REFS], uint32_t *evtchn)
+void *pore_straw_ring_refs(domid_t domid, grant_ref_t refs[NUM_STRAW_REFS])
 {
     int fd = open("/dev/xen/gntalloc", O_RDWR);
-    struct ioctl_gntalloc_alloc_gref op = {
-        .domid = domid,
-        .flags = GNTALLOC_FLAG_WRITABLE,
-        .count = NUM_STRAW_REFS
-    };
 
-    int rs = ioctl(fd, IOCTL_GNTALLOC_ALLOC_GREF, &op);
+    struct ioctl_gntalloc_alloc_gref *op;
+    int size = sizeof(*op) + sizeof(grant_ref_t) *NUM_STRAW_REFS;
+    op = (struct ioctl_gntalloc_alloc_gref *)alloca(size);
+    op->domid = domid;
+    op->flags = GNTALLOC_FLAG_WRITABLE;
+    op->count = NUM_STRAW_REFS;
+
+    int rs = ioctl(fd, IOCTL_GNTALLOC_ALLOC_GREF, op);
     assert(rs == 0);
 
-    void *ring = mmap(0, NUM_STRAW_REFS*PAGE_SIZE,
-          PROT_READ | PROT_WRITE, MAP_SHARED, fd, op.index);
+    void *ring = mmap(0, NUM_STRAW_REFS *PAGE_SIZE,
+                        PROT_READ | PROT_WRITE, MAP_SHARED, fd, op->index);
     assert(ring != MAP_FAILED);
+    printf("mapped: %p\n", ring);
 
     for (int i = 0; i < NUM_STRAW_REFS; i++)
-        refs[i] = op.gref_ids[i];
+        refs[i] = op->gref_ids[i];
     
     close(fd);
 
-    //TODO split into two function
-    //TODO we need to return both fd and event channel number
-
-    //TODO allocate event channel
-    *evtchn = 7;
-
     return ring;
+}
+
+int pore_straw_alloc_unbound(domid_t domid, int fd)
+{
+    struct ioctl_evtchn_bind_unbound_port bind = {
+        .remote_domain = domid,
+    };
+
+    return ioctl(fd, IOCTL_EVTCHN_BIND_UNBOUND_PORT, &bind);
+}
+
+int pore_straw_poke(uint32_t port, int fd)
+{
+    struct ioctl_evtchn_notify notify = {
+        .port = port,
+    };
+
+    return ioctl(fd, IOCTL_EVTCHN_NOTIFY, &notify);
+}
+
+int pore_straw_unbind_port(uint32_t port, int fd)
+{
+    struct ioctl_evtchn_unbind unbind = {
+        .port = port,
+    };
+
+    return ioctl(fd, IOCTL_EVTCHN_UNBIND, &unbind); 
+}
+
+void pore_straw_avail(straw_ring_t *ring, int *ia, int *oa)
+{
+    //TODO: this works for active mode only
+    int avail1 = ring->in_prod - ring->in_cons;
+    while (avail1 < 0)
+        avail1 += STRAW_RING_SIZE;
+    int avail2 = ring->out_cons - ring->out_prod;
+    while (avail2 <= 0)
+        avail2 += STRAW_RING_SIZE;
+    avail2--; // unused byte
+
+    *ia = avail1;
+    *oa = avail2;
+}
+
+int pore_straw_read(straw_ring_t *ring, uint8_t *data, int len)
+{
+    assert(len >= STRAW_RING_SIZE);
+    int prod = ring->in_prod;
+    int cons = ring->in_cons;
+    int avail = prod - cons;
+    while (avail > 0)
+        avail += STRAW_RING_SIZE;
+    //rmb();
+    uint8_t *buffer = ring->input;
+    int read = 0;
+    while (avail-- > 0)
+    {
+        *data++ = buffer[cons++];
+        read++;
+        if (cons >= STRAW_RING_SIZE)
+            cons = 0;
+    }
+    //mb();
+    ring->in_cons = cons;
+    return read;
+}
+
+void pore_straw_write(straw_ring_t *ring, uint8_t *data, int len)
+{
+    int prod = ring->out_prod;
+    int cons = ring->out_cons;
+    //mb();
+    uint8_t *buffer = ring->output;
+    while (len-- > 0)
+    {
+        buffer[prod++] = *data++;
+        if (prod == STRAW_RING_SIZE)
+            prod = 0;
+    }
+    //wmb();
+    ring->out_prod = prod;
 }
 
--- pystraw/straw.py
+++ pystraw/straw.py
@@ -1,4 +1,8 @@
 
+import os
+import select
+import threading
+
 from ctypes import *
 solib = CDLL("pystraw/_pystraw/pystraw.so")
 
@@ -27,30 +31,98 @@ class Straw(object):
     self.my_data_path = "data/straw/%d" % domid
     self.my_state_path = self.my_data_path + "/state"
 
+    self.evtchn_fd = os.open("/dev/xen/evtchn", os.O_RDWR)
+
     xs.write(self.my_state_path, STATE_INITIALISING)
 
   def peer_state_changed(self):
+
     state = self.xs.read(self.peer_state_path)
     if self.is_active and state == STATE_INIT_WAIT:
 
-      ## int pore_straw_open(domid_t domid, grant_ref_t refs[NUM_STRAW_REFS], uint32_t *evtchn)
+      # void *pore_straw_ring_refs(domid_t domid, grant_ref_t refs[NUM_STRAW_REFS])
       refs = (c_uint * NUM_STRAW_REFS)()
-      channel = c_uint()
-      rs = solib.pore_straw_open(self.domid, refs, byref(channel))
-      assert rs == 0
+      solib.pore_straw_ring_refs.restype = c_void_p
+      ring = solib.pore_straw_ring_refs(self.domid, refs)
+      print "ring: 0x%08x" % ring
+      channel = solib.pore_straw_alloc_unbound(self.domid, self.evtchn_fd)
+      print "channel = ", channel
 
       for (i, ref) in enumerate(refs, start=1):
         self.xs.write(self.my_data_path + "/ring-ref-" + str(i), ref)
-      self.xs.write(self.my_data_path + "/event-channel", channel.value)
+      self.xs.write(self.my_data_path + "/event-channel", channel)
+
+      self.ring = ring
+      self.channel = channel
 
       self.xs.write(self.my_state_path, STATE_INITIALISED)
       return True
 
     elif self.is_active and state == STATE_CONNECTED:
+
+      spud = threading.Thread(target=shovel, args=[self])
+      spud.setDaemon(True)
+      spud.start()
+      print "spud started"
+      self.spud = spud
+
       self.xs.write(self.my_state_path, STATE_CONNECTED)
       return True
 
     else:
       print "TODO"
       return True
+
+def shovel(self):
+  ia,oa = update_avail(self.ring)
+  exp_size = None
+
+  while True:
+    print "ia %d oa %d" % ia, oa
+
+    if len(output) > 0 and oa > 0:
+      print "can send %d byte(s)" % oa
+      ch,output = chip(oa, output)
+      # void pore_straw_write(straw_ring_t *ring, uint8_t *data, int len)
+      solib.pore_straw_write(self.ring, c_void_p(ch), len(ch))
+      print "%d byte(s) sent" % len(ch)
+      solib.pore_straw_poke(self.channel, self.evtchn_fd)
+      ia,oa = update_avail(self.ring)
+
+    elif exp_size == None and len(input) >= 4:
+      ch,input = chip(4, input)
+      exp_size, = struct.unpack(">L", ch)
+      print "exp_size = %d" % exp_size
+
+    elif exp_size != None and len(input) >= exp_size:
+      ch,input = chip(exp_size, input)
+      print "deliver ", ch
+      exp_size = None
+
+    elif ia > 0:
+      print "can read %d byte(s)" % ia
+      # int pore_straw_read(straw_ring_t *ring, uint8_t *data, int len)
+      buf = (c_byte * 4096)()
+      len = solib.pore_straw_read(self.ring, buf, 4096)
+      input += str(bytearray(data[:len]))
+      print "%d byte(s) read" % data
+      solib.pore_straw_poke(self.channel, self.evtchn_fd)
+      ia,oa = update_avail(self.ring)
+
+    else:
+      print "waiting for irq"
+      p = select.epoll.fromfd(self.evtchn_fd)
+      select.epoll.poll(p)
+      ia,oa = update_avail(self.ring)
+
+def update_avail(ring):
+    x = c_int()
+    y = c_int()
+    solib.pore_straw_avail(ring, byref(x), byref(y))
+    return x.value,y.value
+
+def chip(n, s):
+    if (n <= len(s)):
+        n = len(s)
+    return s[:n],s[n:]
 
